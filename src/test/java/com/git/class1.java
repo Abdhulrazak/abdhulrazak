4c83e226423112e3f171e5d41818fc98c56140ef
abdhulrazak <abdhulrazak.ag@gmail.com>
2022-12-16 16:38:37 +0530

    Initial codes

---
+++ src/test/java/com/git/class1.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class class1 {
+	
+	private method1() {
+		System.out.println("1");
+	}
+		private method2() {
+			System.out.println("2");
+		}
+		private method3() {
+			System.out.println("3");
+		}
+		
+		
+		
+	}
+
+}
