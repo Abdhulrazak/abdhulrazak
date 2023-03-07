95e91c13de935f5a51eef29059dd20a085782ee5
abdhulrazak <abdhulrazak.ag@gmail.com>
2023-03-07 19:49:52 +0530

    Initial codes

--- src/test/java/com/git/class1.java
+++ src/test/java/com/git/class1.java
@@ -2,13 +2,13 @@ package com.git;
 
 public class class1 {
 	
-	private method1() {
+	private void method1() {
 		System.out.println("1");
 	}
-		private method2() {
+		private void method2() {
 			System.out.println("2");
 		}
-		private method3() {
+		private void method3() {
 			System.out.println("3");
 		}
 		
